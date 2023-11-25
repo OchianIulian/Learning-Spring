ff3c8e3b6d24a70dfce467600d599b9e4329a035
OchianIulian <41990333+OchianIulian@users.noreply.github.com>
2023-11-25 07:02:55 +0200

    Add files via upload

---
+++ second_bkd_project/src/main/java/com/example/second_bkd_project/SecondBkdProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.second_bkd_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecondBkdProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecondBkdProjectApplication.class, args);
+	}
+
+}
---
+++ second_bkd_project/src/test/java/com/example/second_bkd_project/SecondBkdProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.second_bkd_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecondBkdProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
